256b3137a309379e3b4272fe52208988406054aa
San-QP <s.rebollar.g@gmail.com>
2024-01-27 13:03:04 +0100

    Estilos modificados e iconos activos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,18 @@ export const Header = ( ) => {
     return(
     <>
         <header className="headerPage">
+            <img src="../../../public/mosaic/mosaic1-left.svg" alt="decoration header" className="mosaic"/>
             <div className="headerContainer">
-                <h1 className="logo"> I
-                    <span style={{ color: '#FF4F1D' }}>R</span>
-                    <span style={{ color: '#3C65B4' }}>I</span>
+                <h1 className="logo" >
+                    <span style={{ color: '#FF4F1D' }}>I</span>
+                    <span style={{ color: '#3C65B4' }}>R</span>
+                    <span style={{ color: '#E386B6' }}>I</span>
                     <span style={{ color: '#F9CA08' }}>S </span>
                     Weather
                 </h1>
                 <p className="description">The colorful weather APP</p>
             </div>
+            <img src="mosaic/mosaic1-right.svg" alt="decoration header" className="mosaic"/>
         </header>
     </>
     )
--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import "./Panel.css";
 import { getUserLocation } from "../../geolocation";
+import Icons from '../Icons';
 
 export const Panel = () => {
     const [search, setSearch] = useState("");
     const [weatherValues, setWeatherValues] = useState("");
+    const [icon, setIcon] = useState("");
 
+    const currentDate = new Date();
+    const daysOfWeek = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+    const currentDayOfWeek = daysOfWeek[currentDate.getDay()]
+        
     const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&lang=es&units=metric&appid=${import.meta.env.VITE_API_KEY}`;
     
     const getData = async () => {
@@ -17,6 +23,7 @@ export const Panel = () => {
             if (data.cod >= 400) {
                 setWeatherValues(false);
             } else {
+                setIcon(data.list[0].weather[0].main);
                 setWeatherValues(data);
             }
         })
@@ -24,7 +31,6 @@ export const Panel = () => {
             console.error("Error al obtener datos de la API:", error);
         });
     };
-
     
     const handleSearch = (e) => {
         if (e.key === "Enter") {
@@ -36,10 +42,6 @@ export const Panel = () => {
         setSearch(value);
     }
     
-    const currentDate = new Date();
-    const daysOfWeek = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-    const currentDayOfWeek = daysOfWeek[currentDate.getDay()]
-    
     useEffect(() => {
         getUserLocation()
             .then((city) => {
@@ -50,7 +52,6 @@ export const Panel = () => {
             });
         },        
         []);
-
         
     useEffect(() => {
         getData();
@@ -58,64 +59,80 @@ export const Panel = () => {
         }, [search]);        
 
     return (
-    <section className="panel">
-        <div className="divider">
-            <div className="deepBlue"></div>
-            <div className="orange"></div>
-            <div className="blue"></div>
-            <div className="yellow"></div>
-        </div>
-        <div className="searchContainer">
-            <input
-                onKeyDown={handleSearch}
-                type="text"
-                placeholder="Busca una localización"
-                autoFocus
-            />
-            <div
-                className="searchButton"
-                onClick={() => handleButtonSearch(document.querySelector('input').value)}
-            >
-                Buscar
-            </div>
-        </div>
-
-        <div className="card">
-            {weatherValues ? (
-            <div className="cardContainer">
-                <div className="circle">
-                    <span className="material-symbols-sharp">location_on</span>
+    <main className="mainContent">
+        <section className="columnMosaic">
+            <img src="/public/mosaic/mosaic2-left.svg" alt="mosaic decoration" className="mosaic"/>
+            <img src="/public/mosaic/mosaic3-left.svg" alt="mosaic decoration" className="mosaic"/>
+            <img src="/public/mosaic/mosaic4-left.svg" alt="mosaic decoration" className="mosaic"/>
+        </section>
+        <section className="panel">
+            <div className="searchContainer">
+                <input
+                    onKeyDown={handleSearch}
+                    type="text"
+                    placeholder="El tiempo en..."
+                    autoFocus
+                />
+                <div
+                    className="searchButton"
+                    onClick={() => handleButtonSearch(document.querySelector('input').value)}
+                >
+                    Buscar
                 </div>
-                <h2 className="cityName">{weatherValues.city.name}</h2>
-                <p className="date">
-                    {`${currentDayOfWeek}, ${weatherValues.list[0].dt_txt.substring(0, weatherValues.list[0].dt_txt.length - 9)}`}
-                </p>
-                <p className="weather">
-                    {weatherValues.list[0].weather[0].description}
-                </p>
-                <div className="info">
-                    <div className="temp">
-                        <p className="mainTemp">
-                            {weatherValues.list[0].main.temp.toFixed(0)}&deg;
-                        </p>
-                        <div className="card-footer">
-                            <p className="temp-max-min">
-                                {weatherValues.list[0].main.temp_min.toFixed(0)}&deg; |{" "}
-                                {weatherValues.list[0].main.temp_max.toFixed(0)}&deg;
-                            </p>
-                        </div>
-                    </div>
-                    <img
-                        className="iconWeather"
-                        src={`https://openweathermap.org/img/wn/${weatherValues.list[0].weather[0].icon}.png`}
-                        alt="icon-weather"
-                    />
-                    </div>
             </div>
-            ) : (
-            <h2 className="cityName">{"Ubicación no disponible"}</h2>
-            )}
-        </div>
-    </section>
+
+            <div className="weatherInfoContainer">
+                {weatherValues ? (
+                <div>
+                    <ul className="locationDate">
+                        <li className="location">
+                            <div className="circle">
+                                <span className="material-symbols-sharp">location_on</span>
+                            </div>
+                        <h2 className="cityName">{weatherValues.city.name}</h2>
+                        </li>
+                        <li className="date">
+                            {`${currentDayOfWeek}, ${weatherValues.list[0].dt_txt.substring(0, weatherValues.list[0].dt_txt.length - 9)}`}
+                        </li>
+                    </ul>
+                    <ul className="tempWeather">
+                        <li className="temp">
+                            <p className="mainTemp">
+                                {weatherValues.list[0].main.temp.toFixed(0)}&deg;
+                            </p>
+                            <div className="tempDetails">
+                                <p className="tempMaxMin">
+                                    mín. {weatherValues.list[0].main.temp_min.toFixed(0)}&deg; |{" "}
+                                    máx. {weatherValues.list[0].main.temp_max.toFixed(0)}&deg;
+                                </p>
+                            </div>
+                            <div className="detailsButton">
+                                Más detalles
+                            </div>
+                        </li>
+                        <li className="mainWeather">
+                            <img
+                                className="iconWeather"
+                                src={Icons(icon)}
+                                alt="icon-weather"
+                            />
+                            <p className="weather">
+                                {weatherValues.list[0].weather[0].description}
+                            </p>
+                        </li>
+                    </ul>
+                    
+                </div>
+                ) : (
+                <h2 className="cityName">{"Ubicación no disponible"}</h2>
+                )}
+            </div>
+        </section>
+        <section className="columnMosaic">
+            <img src="/public/mosaic/mosaic2-right.svg" alt="mosaic decoration" className="mosaic"/>
+            <img src="/public/mosaic/mosaic3-right.svg" alt="mosaic decoration" className="mosaic"/>
+            <img src="/public/mosaic/mosaic4-right.svg" alt="mosaic decoration" className="mosaic"/>
+        </section>
+    </main>
     );
 };
